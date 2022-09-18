fb6cb34911446d9dfa8bf78c00dfb756ccf08980
farrar142 <farrar142@github.com>
2022-09-18 17:17:18 +0900

    fix:인자명 변경

--- src/bot/MessageInteraction.ts
+++ src/bot/MessageInteraction.ts
@@ -3,19 +3,19 @@ import User from 'src/backend/entity/User';
 import Message from 'src/backend/entity/Message';
 import client from 'src/bot/client';
 
-const MessageInteraction = async (interaction: Discord.Message) => {
+const MessageInteraction = async (message: Discord.Message) => {
 	// NOTE: PREFIX를 커스텀으로 설정 할 수 있음
-	const command = client.getCommandFromMessage(interaction);
-	const user = await User.createOrGetFromInteraction(interaction);
-	Message.createFromInteraction(interaction);
+	const command = client.getCommandFromMessage(message);
+	const user = await User.createOrGetFromInteraction(message);
+	Message.createFromInteraction(message);
 
 	if (!command.acceptable) return;
 
 	if (command.command == '야옹해봐') {
-		interaction.channel.send('에옹?');
+		message.channel.send('에옹?');
 		return;
 	}
-	interaction.reply('야옹');
+	message.reply('야옹');
 };
 
 export default MessageInteraction;
