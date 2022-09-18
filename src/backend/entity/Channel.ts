302fedbaf79b02327842935d2b3c2b05828a675d
farrar142 <farrar142@github.com>
2022-09-18 19:08:23 +0900

    feat:기본 모델들에 메세지에서 인스턴스를 가져오는 메소드 추가

--- src/backend/entity/Channel.ts
+++ src/backend/entity/Channel.ts
@@ -1,10 +1,37 @@
+import Discord from 'discord.js';
 import { Column, Entity } from 'typeorm';
 import BaseModel from 'src/backend/entity/BaseModel';
 
 @Entity()
 class Channel extends BaseModel {
-	@Column()
+	@Column({ default: true })
 	permission: boolean;
+
+	static getOrCreate(arg: { id: number; name: string }) {
+		return new Promise<Channel>((resolve, reject) => {
+			Channel.findOneByOrFail({ id: arg.id })
+				.then(resolve)
+				.catch(async () => {
+					const instance = Channel.create<Channel>(arg);
+					const savedInstance = await instance.save();
+					resolve(savedInstance);
+				});
+		});
+	}
+
+	static getOrCreateFromMessage(message: Discord.Message) {
+		const channel = message.channel;
+		const guild_id = parseInt(channel.id, 10);
+		let name: string;
+		if (channel.isDMBased()) {
+			name = 'default';
+		} else {
+			name = channel.name;
+		}
+		return new Promise<Channel>((resolve, reject) => {
+			Channel.getOrCreate({ id: guild_id, name: name }).then(resolve).catch(reject);
+		});
+	}
 }
 
 export default Channel;
