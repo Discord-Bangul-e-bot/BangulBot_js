9f01c403483ae221baf18d082279a586a2412788
farrar142 <farrar142@github.com>
2022-09-20 00:34:00 +0900

    fix:리포지터리로 이전된 메소드 삭제

--- src/backend/entity/Channel.ts
+++ src/backend/entity/Channel.ts
@@ -8,38 +8,7 @@ class Channel extends BaseModel {
 	@Column({ default: true })
 	permission: boolean;
 
-	static getOrCreate(args: { id: string; name: string }) {
-		return new Promise<Channel>((resolve, reject) => {
-			Channel.findOneByOrFail({ id: args.id })
-				.then(async (channel) => {
-					if (channel.name !== args.name) {
-						await channel.setName(args.name);
-					}
-					resolve(channel);
-				})
-				.catch(async () => {
-					const instance = Channel.create<Channel>(args);
-					const savedInstance = await instance.save();
-					resolve(savedInstance);
-				});
-		});
-	}
-
-	static getOrCreateFromMessage(message: MessageBase) {
-		const channel = message.channel;
-		const guild_id = channel.id;
-		let name: string;
-		if (channel.isDMBased()) {
-			name = 'default';
-		} else {
-			name = channel.name;
-		}
-		return new Promise<Channel>((resolve, reject) => {
-			Channel.getOrCreate({ id: guild_id, name: name }).then(resolve).catch(reject);
-		});
-	}
-
-	private async setName(name: string) {
+	async setName(name: string) {
 		this.name = name;
 		return this.save();
 	}
