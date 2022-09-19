346e8ab64a1a5fdc8796fca07db2f4b863f2004e
farrar142 <farrar142@github.com>
2022-09-20 00:31:49 +0900

    fix:채널 리포지터리 작성

---
+++ src/backend/repository/ChannelRepository.ts
@@ -0,0 +1,52 @@
+import Channel from 'src/backend/entity/Channel';
+import { RepositoryDecorator } from 'src/backend/repository';
+import { BaseModelQueryParam, MessageBase } from 'src/backend/types';
+import { DEFAULTCATNAME } from 'src/const';
+
+@RepositoryDecorator<Channel>()
+class UserRepository {
+	model: Channel;
+
+	public static getOrCreate(args: BaseModelQueryParam): Promise<Channel> {
+		return new Promise<Channel>((resolve, reject) => {
+			Channel.findOneByOrFail({ id: args.id })
+				.then(async (model) => {
+					if (this.name !== args.name) {
+						await model.setName(args.name);
+					}
+					resolve(model);
+				})
+				.catch(async () => {
+					const instance = Channel.create<Channel>({ id: args.id, name: DEFAULTCATNAME });
+					const savedInstance = await instance.save();
+					resolve(savedInstance);
+				});
+		});
+	}
+
+	public static getOrCreateFromMessage(message: MessageBase): Promise<Channel> {
+		const channel = message.channel;
+		const channel_id = channel.id;
+		let name: string;
+		if (channel.isDMBased()) {
+			name = 'default';
+		} else {
+			name = channel.name;
+		}
+		return new Promise<Channel>((resolve, reject) => {
+			UserRepository.getOrCreate({ id: channel_id, name: name }).then(resolve).catch(reject);
+		});
+	}
+
+	getName() {
+		return this.model.name;
+	}
+
+	async setName(name: string) {
+		this.model.name = name;
+		await this.model.save();
+		return;
+	}
+}
+
+export default UserRepository;
