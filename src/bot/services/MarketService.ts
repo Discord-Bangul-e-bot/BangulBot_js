19b823baaa2d3e0f368401b8be26db6259280a26
farrar142 <farrar142@github.com>
2022-10-02 03:45:55 +0900

    feat:인터랙션서비스의 기능을 일부 담당하는 마켓서비스 생성

---
+++ src/bot/services/MarketService.ts
@@ -0,0 +1,57 @@
+import Discord from 'discord.js';
+import Cat from 'src/backend/entity/Cat';
+import Channel from 'src/backend/entity/Channel';
+import Message from 'src/backend/entity/Message';
+import Relation from 'src/backend/entity/Relation';
+import User from 'src/backend/entity/User';
+import CatRepository from 'src/backend/repository/CatRepository';
+import InventoryRepository from 'src/backend/repository/InventoryRepository';
+import ItemRepository from 'src/backend/repository/ItemRepository';
+import { InteractionCreateDTO } from 'src/bot/InteractionService';
+import Formatter from 'src/bot/util/formatter';
+
+class MarketService {
+	formatter: typeof Formatter = Formatter;
+	cat: Cat;
+	channel: Channel;
+	user: User;
+	relation: Relation;
+	message?: Message;
+	interaction: Discord.Interaction;
+
+	constructor(args: InteractionCreateDTO) {
+		this.user = args.user;
+		this.cat = args.cat;
+		this.channel = args.channel;
+		this.relation = args.relation;
+		this.message = args.message;
+	}
+
+	async openStore() {
+		const items = await ItemRepository.getItems();
+		return items;
+	}
+
+	async myItems() {
+		return InventoryRepository.findAll(this.user);
+	}
+
+	async findItem(name: string) {
+		return InventoryRepository.findItem(this.user, name);
+	}
+
+	async buyItem(name: string) {
+		const itemRepo = await ItemRepository.buyItem(name, this.user);
+		if (!itemRepo) {
+			return false;
+		}
+		return true;
+	}
+
+	async useItem(itemRepo: InventoryRepository) {
+		await itemRepo.useItem();
+		await new CatRepository(this.cat);
+	}
+}
+
+export default MarketService;
